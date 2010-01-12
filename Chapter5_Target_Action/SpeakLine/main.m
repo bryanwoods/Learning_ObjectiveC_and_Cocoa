4d2f7a57d3dfc3367d0d44714fa86755e976c1c7
Bryan Woods <bryanwoods4e@gmail.com>
2010-01-12 02:13:23 -0500

    Adding skeleton project for Chapter 5

---
+++ Chapter5_Target_Action/SpeakLine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpeakLine
+//
+//  Created by Bryan Woods on 1/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
