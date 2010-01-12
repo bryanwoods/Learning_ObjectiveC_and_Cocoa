3fc15a6b807ad93d7b5e8f4c094405e4d6d97932
Bryan Woods <bryanwoods4e@gmail.com>
2010-01-11 21:22:54 -0500

    Updating lottery.m to use the LotteryEntry class

--- Chapter3_Objective_C/lottery/lottery.m
+++ Chapter3_Objective_C/lottery/lottery.m
@@ -1,20 +1,34 @@
 #import <Foundation/Foundation.h>
+#import "LotteryEnry.h"
 
 int main (int argc, const char * argv[])
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	
+	NSCalendarDate *now = [[NSCalendarDate alloc] init];
+	
+	srandom(time(NULL));
 	NSMutableArray *array;
 	array = [[NSMutableArray alloc] init];
+	
 	int i;
 	for (i = 0; i < 10; i++) {
-		NSNumber *newNumber = [[NSNumber alloc] initWithInt:(i * 3)];
-		[array addObject:newNumber];
+		NSCalendarDate *iWeeksFromNow;
+		iWeeksFromNow = [now dateByAddingYears:0 
+										months:0
+										  days:(i * 7) 
+										 hours:0
+									   minutes:0
+									   seconds:0];
+		
+		LotteryEntry *newEntry = [[LotteryEntry alloc] init];
+		[newEntry prepareRandomNumbers];
+		[newEntry setEntryDate:iWeeksFromNow];
+		[array addObject:newEntry];
 	}
 	
 	for (i = 0; i < 10; i++) {
-		NSNumber *numberToPrint = [array objectAtIndex:i];
-		NSLog(@"The number at index %d is %@", i, numberToPrint);
+		NSLog(@"%@", entryToPrint);
 	}
 	
 	[pool drain];
