273a673cd56708ec46679ec11292be316eae134e
Bryan Woods <bryanwoods4e@gmail.com>
2010-01-13 00:33:26 -0500

    Adding Speakline project to new Chapter 6 directory

---
+++ peepcode_objectivec_for_rubyists/PeepNews/SyntaxTest.h
@@ -0,0 +1,16 @@
+//
+//  SyntaxTest.h
+//  PeepNews
+//
+//  Created by Bryan Woods on 1/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface SyntaxTest : SenTestCase {
+
+}
+
+@end
---
+++ peepcode_objectivec_for_rubyists/PeepNews/SyntaxTest.m
@@ -0,0 +1,14 @@
+//
+//  SyntaxTest.m
+//  PeepNews
+//
+//  Created by Bryan Woods on 1/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SyntaxTest.h"
+
+
+@implementation SyntaxTest
+
+@end
